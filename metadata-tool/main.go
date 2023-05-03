0d4c06f01eb56be0373f4a78d394418c64d4bdf1
SeongGyu Park <tawny.port@kakaoenterprise.com>
2023-05-03 17:54:04 +0900

    refactoring metadata tool

--- metadata-tool/main.go
+++ metadata-tool/main.go
@@ -10,15 +10,19 @@ import (
 	"github.com/lolgopher/synology-filesync/tool"
 )
 
+var rootPath string
+
 func main() {
-	rootPath, _ := os.Getwd()
+	rootPath, _ = os.Getwd()
 
 	var (
 		targetPath      string
+		failed          bool
 		failedToNotSent bool
 		removeZeroSize  bool
 	)
 	flag.StringVar(&targetPath, "path", rootPath, "Target Path to search metadata files")
+	flag.BoolVar(&failed, "failed", false, "Search failed status")
 	flag.BoolVar(&failedToNotSent, "retry", false, "Change failed to notsent status")
 	flag.BoolVar(&removeZeroSize, "zero", false, "Remove zero size and notsent status")
 
@@ -31,27 +35,41 @@ func main() {
 	log.Printf("retry: %v", failedToNotSent)
 	log.Printf("zero: %v", removeZeroSize)
 
+	if failed {
+		if result, err := searchFailedStatus(targetPath, failedToNotSent); err != nil {
+			log.Fatal(err)
+		} else {
+			log.Print(result)
+		}
+	}
+
 	if removeZeroSize {
 
-	} else {
-		data, err := tool.GetFailedStatus(targetPath)
-		if err != nil {
-			log.Fatal(err)
-		}
+	}
+}
 
-		rootPath, err = filepath.Abs(filepath.Join(targetPath, ".."))
-		if err != nil {
-			log.Fatal(err)
-		}
+func searchFailedStatus(targetPath string, failedToNotSent bool) (string, error) {
+	result := "\n"
 
-		for key := range data {
-			log.Println(key)
+	data, err := tool.GetFailedStatus(targetPath)
+	if err != nil {
+		return "", err
+	}
 
-			if failedToNotSent {
-				if err := protocol.WriteMetadata(filepath.Join(rootPath, key), 0, protocol.NotSent); err != nil {
-					log.Fatal(err)
-				}
+	rootPath, err = filepath.Abs(filepath.Join(targetPath, ".."))
+	if err != nil {
+		return "", err
+	}
+
+	for key := range data {
+		result += key + "\n"
+
+		if failedToNotSent {
+			if err := protocol.WriteMetadata(filepath.Join(rootPath, key), 0, protocol.NotSent); err != nil {
+				return "", err
 			}
 		}
 	}
+
+	return result, nil
 }
