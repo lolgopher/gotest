43c4cd6f20c39382e2191a46c008fb8213abb44a
SeongGyu Park <tawny.port@kakaoenterprise.com>
2023-05-03 17:38:11 +0900

    add zero flag in metadata tool

--- metadata-tool/main.go
+++ metadata-tool/main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"flag"
-	"github.com/lolgopher/synology-filesync/protocol"
-	"github.com/lolgopher/synology-filesync/tool"
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/lolgopher/synology-filesync/protocol"
+	"github.com/lolgopher/synology-filesync/tool"
 )
 
 func main() {
 	rootPath, _ := os.Getwd()
 
-	var targetPath string
-	var failedToNotSent bool
+	var (
+		targetPath      string
+		failedToNotSent bool
+		removeZeroSize  bool
+	)
 	flag.StringVar(&targetPath, "path", rootPath, "Target Path to search metadata files")
 	flag.BoolVar(&failedToNotSent, "retry", false, "Change failed to notsent status")
+	flag.BoolVar(&removeZeroSize, "zero", false, "Remove zero size and notsent status")
 
 	flag.Parse()
 	targetPath, err := filepath.Abs(filepath.Join(".", targetPath))
@@ -24,23 +29,28 @@ func main() {
 	}
 	log.Printf("path: %v", targetPath)
 	log.Printf("retry: %v", failedToNotSent)
+	log.Printf("zero: %v", removeZeroSize)
 
-	data, err := tool.GetFailedStatus(targetPath)
-	if err != nil {
-		log.Fatal(err)
-	}
+	if removeZeroSize {
 
-	rootPath, err = filepath.Abs(filepath.Join(targetPath, ".."))
-	if err != nil {
-		log.Fatal(err)
-	}
+	} else {
+		data, err := tool.GetFailedStatus(targetPath)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	for key := range data {
-		log.Println(key)
+		rootPath, err = filepath.Abs(filepath.Join(targetPath, ".."))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		if failedToNotSent {
-			if err := protocol.WriteMetadata(filepath.Join(rootPath, key), 0, protocol.NotSent); err != nil {
-				log.Fatal(err)
+		for key := range data {
+			log.Println(key)
+
+			if failedToNotSent {
+				if err := protocol.WriteMetadata(filepath.Join(rootPath, key), 0, protocol.NotSent); err != nil {
+					log.Fatal(err)
+				}
 			}
 		}
 	}
