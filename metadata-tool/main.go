e8f6327b5b824e6f02b6f9c82a2c3924ed4c3d3b
SeongGyu Park <tawny.port@kakaoenterprise.com>
2023-05-11 15:32:10 +0900

    update metadata tool

--- metadata-tool/main.go
+++ metadata-tool/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"github.com/lolgopher/synology-filesync/protocol"
+	"github.com/lolgopher/synology-filesync/tool"
+	"gopkg.in/yaml.v2"
 	"log"
 	"os"
 	"path/filepath"
-
-	"github.com/lolgopher/synology-filesync/protocol"
-	"github.com/lolgopher/synology-filesync/tool"
 )
 
 var rootPath string
@@ -47,6 +48,11 @@ func main() {
 	}
 
 	if notSent {
+		if result, err := searchNotSentStatus(targetPath, removeZeroSize); err != nil {
+			log.Fatal(err)
+		} else {
+			log.Print(result)
+		}
 	}
 }
 
@@ -69,6 +75,51 @@ func searchFailedStatus(targetPath string, failedToNotSent bool) (string, error)
 		if failedToNotSent {
 			if err := protocol.WriteMetadata(filepath.Join(rootPath, key), 0, protocol.NotSent); err != nil {
 				return "", err
+			}
+
+func searchNotSentStatus(targetPath string, removeZeroSize bool) (string, error) {
+	result := "\n"
+
+	data, err := tool.GetNotSentStatus(targetPath)
+	if err != nil {
+		return "", err
+	}
+
+	targetRootPath, err := filepath.Abs(filepath.Join(targetPath, ".."))
+	if err != nil {
+		return "", err
+	}
+
+	for key := range data {
+		result += key + "\n"
+
+		if removeZeroSize {
+			// 메타데이터 파일 읽기
+			metadataPath := filepath.Dir(filepath.Join(targetRootPath, key))
+			//if strings.Count(metadataPath, "/volume2/docker") > 1 {
+			//	metadataPath = strings.Replace(metadataPath, "/volume2/docker", "", 1)
+			//}
+			meta, err := protocol.ReadMetadata(metadataPath)
+			if err != nil {
+				return "", err
+			}
+
+			if _, ok := meta[key]; !ok {
+				return "", fmt.Errorf("fail to find %s in %s metadata file", key, metadataPath)
+			}
+			if meta[key].Size == 0 {
+				// 메타데이터 삭제
+				delete(meta, key)
+
+				// 메타데이터 파일 쓰기
+				metadataData, err := yaml.Marshal(meta)
+				if err != nil {
+					return "", fmt.Errorf("fail to marshal %v data: %v", meta, err)
+				}
+				metadataFilePath := filepath.Join(metadataPath, "metadata.yaml")
+				if err := os.WriteFile(metadataFilePath, metadataData, 0644); err != nil {
+					return "", fmt.Errorf("fail to write %s file: %v", metadataFilePath, err)
+				}
 			}
 		}
 	}
