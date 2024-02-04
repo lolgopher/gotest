3fb6868a9bd36d0b2b889ae7d321cf7deeb249c9
lolgopher <psk5024@gmail.com>
2024-02-04 22:09:12 +0900

    add case of error compare

--- errors-compare/go.mod
+++ errors-compare/go.mod
@@ -1,3 +1,5 @@
 module github.com/lolgopher/gotest/errors-compare
 
 go 1.20
+
+require github.com/pkg/errors v0.9.1 // indirect
--- errors-compare/main.go
+++ errors-compare/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	UsecaseErrIndureUDPTransportFail = errors.New("indure udp transport fail")
+	DtoErrIndureUDPTransportFail     = errors.New("indure udp transport fail")
 )
 
 type MyError struct {
@@ -29,13 +35,60 @@ func (e *MyError) Unwrap() error {
 
 func main() {
 	err := someFunction()
+	myErr := &MyError{}
 	if errors.Is(err, &SomeOtherError{}) {
 		fmt.Println("SomeOtherError occurred")
-	} else if errors.As(err, MyError{}) {
+	} else if errors.As(err, &myErr) {
 		fmt.Println("MyError occurred")
 	} else {
 		fmt.Println("Unknown error occurred")
 	}
+	fmt.Println()
+
+	err = UsecaseErrIndureUDPTransportFail
+	fmt.Println(err)
+
+	if errors.Is(err, UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("UsecaseErrIndureUDPTransportFail errors.Is match")
+	}
+	if errors.As(err, &UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("UsecaseErrIndureUDPTransportFail errors.As match")
+	}
+	fmt.Println()
+
+	err = DtoErrIndureUDPTransportFail
+	fmt.Println(err)
+
+	if errors.Is(err, UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("DtoErrIndureUDPTransportFail errors.Is match")
+	}
+	if errors.As(err, &UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("DtoErrIndureUDPTransportFail errors.As match")
+	}
+	fmt.Println()
+
+	err = errors.Wrap(DtoErrIndureUDPTransportFail, "fail to send rtp packet")
+	fmt.Println(err)
+
+	if errors.Is(err, UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("Wrapped DtoErrIndureUDPTransportFail errors.Is match")
+	}
+	if errors.As(err, &UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("Wrapped DtoErrIndureUDPTransportFail errors.As match")
+	}
+	fmt.Println()
+
+	err = errors.Wrap(DtoErrIndureUDPTransportFail, "write: operation not permitted")
+	err = errors.Wrap(err, "fail to send rtp packet")
+	fmt.Println(err)
+
+	if errors.Is(err, UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("Two Wrapped DtoErrIndureUDPTransportFail errors.Is match")
+	}
+	if errors.As(err, &UsecaseErrIndureUDPTransportFail) {
+		fmt.Println("Two Wrapped DtoErrIndureUDPTransportFail errors.As match")
+	}
+	fmt.Println()
 }
 
 func someFunction() error {
